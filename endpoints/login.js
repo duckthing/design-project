85efa826f6a4e337ef2dc145c8f6ae6646fa2607
jahreddobranic <58601983+jahreddobranic@users.noreply.github.com>
2024-10-18 21:38:40 -0500

    modified login and register to pass unit test cases

--- endpoints/login.js
+++ endpoints/login.js
@@ -1,24 +1,27 @@
 const accountsModule = require("../src/accounts");
 const bodyParser = require('body-parser');
 
-exports.get = function(req, res) {
-	res.render("./pages/login.ejs", {});
-}
+exports.get = function (req, res) {
+  res.render("./pages/login.ejs", {});
+};
 
-exports.post = function(req, res) {
-	if (req.body == null || req.body.username == null || req.body.password == null) {
-		res.send("Invalid data");
-		return;
-	}
-	const account = accountsModule.getUserAccount(req.body.username);
-	if (account == null) {
-		res.send("Invalid username");
-		return;
-	}
-	if (account.password != req.body.password.trim()) {
-		res.send("Invalid password");
-		return;
-	}
-	req.session.user = {username: req.body.username};
-	res.redirect("user/user-profile-management");
+exports.post = function (req, res) {
+  if (!req.body || !req.body.username || !req.body.password) {
+    res.status(400).send("Invalid data");
+    return;
+  }
+
+  const account = accountsModule.getUserAccount(req.body.username);
+  if (!account) {
+    res.status(401).send("Invalid username");
+    return;
+  }
+
+  if (account.password !== req.body.password.trim()) {
+    res.status(401).send("Invalid password");
+    return;
+  }
+
+  req.session.user = { username: req.body.username };
+  res.redirect("/user/user-profile-management");
 }
