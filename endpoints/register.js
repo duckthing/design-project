586851c851aa5cd7d4add115e476418819db3a04
jahreddobranic <58601983+jahreddobranic@users.noreply.github.com>
2024-10-18 20:36:16 -0500

    added registration module

--- endpoints/register.js
+++ endpoints/register.js
@@ -1,3 +1,24 @@
+const accountsModule = require("../src/accounts");
 exports.get = function(req, res) {
 	res.render("./pages/register.ejs", {});
+}
+exports.post = function(req, res) {
+	if (req.body == null || req.body.role == null || req.body.fullname == null || req.body.email == null || req.body.email == null) {
+		res.send("Invalid data");
+		return;
+	}
+	const username = req.body.username;
+	const password = req.body.password;
+	const fullName = req.body.fullName;
+	const address1 = req.body.address1;
+	const address2 = req.body.address2;
+	const city = req.body.city;
+	const state = req.body.state;
+	const skills = [];
+	const preferences = req.body.preferences;
+	const availability = req.body.availability;
+
+	let success ,account = accountsModule.createUserAccount(username, password, fullName, address1, address2, city, state, skills, preferences, availability);
+	req.session.user = {username: req.body.username};
+	res.redirect("user/user-profile-management");
 }
